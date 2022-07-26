3a0b584ff1ad5b3be1cc5bf42f51dd9f91e10e6c
Victoria Fields <victoriaashleyfields@gmail.com>
2022-07-26 17:56:38 -0600

    Add clearInput event handler to save button, refactoring addIdea handler

--- main.js
+++ main.js
@@ -8,6 +8,26 @@ var userBodyInput = document.querySelector("#body-input")
 var ideaboxSection = document.querySelector(".ideabox-section")
 var showStarredButton = document.querySelector(".showStarredButton")
 
-
-
 //FUNCTIONS:
+
+saveButton.addEventListener('click', addIdea);
+saveButton.addEventListener('click', clearInput);
+
+function addIdea() {
+  if (userTitleInput.value.length > 0 && userBodyInput.value.length > 0) {
+    var newIdea = new Idea(userTitleInput.value, userBodyInput.value);
+    ideas.push(newIdea);
+    console.log(ideas);
+    // displayIdeas(); I should see a new idea card with the provided title and body appear on the DOM
+  }
+  else if (userTitleInput.value.length === 0 || userBodyInput.value.length === 0) {
+    console.log('else if fired');
+    
+    // alert('You need to fill out the Title and Body sections to save an idea!');
+  }
+}
+
+function clearInput() {
+  userTitleInput.value = '';
+  userBodyInput.value = '';
+}
