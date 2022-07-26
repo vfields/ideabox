ab120e1fb0b923335cc5aabc827c1d1532db7ed6
Alycia Canavan <alycia.canavan@gmail.com>
2022-07-26 15:50:40 -0700

    Add createNewIdea function Creates and renders an idea card, does not yet clear input fields or disable save button.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 //GLOBAL VARIABLE:
 var ideas = []
 
@@ -8,6 +10,30 @@ var userBodyInput = document.querySelector("#body-input")
 var ideaboxSection = document.querySelector(".ideabox-section")
 var showStarredButton = document.querySelector(".showStarredButton")
 
-
+//EVENT LISTENERS:
+saveButton.addEventListener("click", createNewIdea)
 
 //FUNCTIONS:
+
+
+function createNewIdea() {
+  var newIdea = new Idea(userTitleInput.value, userBodyInput.value)
+  ideas.push(newIdea)
+  var newIdeaElement = document.createElement("div")
+  ideaboxSection.appendChild(newIdeaElement)
+  newIdeaElement.classList.add("ideabox-container")
+  newIdeaElement.innerHTML = `<div class="ideabox-container">
+    <div class="ideabox-header">
+      <div class="ideabox-header-image"><img src="assets/star-active.svg" alt="star to favorite"></div>
+      <div class="ideabox-header-image"><img src="assets/delete.svg" alt="x to delete"></div>
+    </div>
+    <div class="ideabox-body">
+      <h3>${newIdea.title}</h3>
+      <p class="ideabox-body-text">${newIdea.body}</p>
+    </div>
+    <div class="ideabox-footer">
+      <div class="ideabox-footer-image"><img src="assets/comment.svg"></div>
+      <div class="ideabox-comment"><p>Comment</p></div>
+    </div>
+  </div>`
+}
