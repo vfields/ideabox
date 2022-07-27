c88c6c4ae9e08e4dd1c73490c16e1a4964a3535b
Victoria Fields <victoriaashleyfields@gmail.com>
2022-07-27 10:19:12 -0600

    Debug and refactor save button display, refactor addIdea function

--- main.js
+++ main.js
@@ -12,16 +12,14 @@ var formContainer = document.querySelector(".form-container");
 //FUNCTIONS:
 
 saveButton.addEventListener('click', addIdea);
-saveButton.addEventListener('click', clearInput);
 formContainer.addEventListener('input', saveButtonDisplay);
+// saveButton.addEventListener('click', clearInput);
 
 function addIdea() {
-  if (userTitleInput.value.length > 0 && userBodyInput.value.length > 0) {
-    var newIdea = new Idea(userTitleInput.value, userBodyInput.value);
-    ideas.push(newIdea);
-    // console.log(ideas);
+  var newIdea = new Idea(userTitleInput.value, userBodyInput.value);
+  ideas.push(newIdea);
 
-    ideaboxSection.innerHTML += `
+  ideaboxSection.innerHTML += `
     <div class="ideabox-container">
       <div class="ideabox-header">
         <div class="ideabox-header-image"><img src="assets/star-active.svg" alt="star to favorite"></div>
@@ -37,14 +35,6 @@ function addIdea() {
       </div>
     </div>
     `
-  }
-  // else if (userTitleInput.value.length === 0 || userBodyInput.value.length === 0) {
-  //   console.log('else if fired')
-  //   alert('You need to fill out the Title and Body sections to save an idea!');
-  // }
-}
-
-function clearInput() {
   saveButton.disabled = true;
   if (userTitleInput.value.length > 0 && userBodyInput.value.length > 0) {
     userTitleInput.value = '';
@@ -53,11 +43,18 @@ function clearInput() {
 }
 
 function saveButtonDisplay() {
-  // console.log('this is an input event');
   if (userTitleInput.value.length > 0 && userBodyInput.value.length > 0) {
     saveButton.disabled = false;
   }
-  // else {
-  //   saveButton.disabled = true; // I think this is redundant, since we reset the boolean in the clearInput function
-  // }
+  else {
+    saveButton.disabled = true;
+  }
 }
+
+// function clearInput() {
+//   saveButton.disabled = true;
+//   if (userTitleInput.value.length > 0 && userBodyInput.value.length > 0) {
+//     userTitleInput.value = '';
+//     userBodyInput.value = '';
+//   }
+// }
